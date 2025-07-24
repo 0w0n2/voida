c3449c545b66ecd9238b964ca15c3bb1b7b8ad72
Gyuchan Kim <sb3g@naver.com>
2025-07-24 09:38:34 +0900

    [BE] init: 프로젝트 초기 구조 생성

---
+++ backend-core/src/test/java/com/bbusyeo/voida/VoidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbusyeo.voida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
