244645a6ba1c9d56fac1f35d7122839e672466e5
Gyuchan Kim <sb3g@naver.com>
2025-07-26 20:32:49 +0900

    [BE] feat: API 초기 구성 및 에러 핸들링 정의

---
+++ backend-core/src/main/java/com/bbusyeo/voida/global/response/BaseResponse.java
@@ -0,0 +1,28 @@
+package com.bbusyeo.voida.global.response;
+
+import static com.bbusyeo.voida.global.response.BaseResponseStatus.SUCCESS;
+import org.springframework.http.HttpStatus;
+import com.bbusyeo.voida.global.support.TypeCaster;
+
+public record BaseResponse<T>(HttpStatus httpStatus, Boolean isSuccess, String message, int code,
+                              T result) {
+
+    public BaseResponse(T result) {
+        this(HttpStatus.OK, true, SUCCESS.getMessage(), SUCCESS.getCode(), result);
+    }
+
+    public BaseResponse() {
+        this(HttpStatus.OK, true, SUCCESS.getMessage(), SUCCESS.getCode(), null);
+    }
+
+    public BaseResponse(BaseResponseStatus status) {
+        this(status.getHttpStatus(), false, status.getMessage(), status.getCode(),
+            TypeCaster.castMessage(status.getMessage()));
+    }
+
+    public BaseResponse(BaseResponseStatus status, String message) {
+        this(status.getHttpStatus(), false, message, status.getCode(),
+            TypeCaster.castMessage(status.getMessage()));
+    }
+
+}
