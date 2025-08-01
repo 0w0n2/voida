3797d84b6e81d431797edcb6c8680b5134f22746
LEESEOKJAE <lsj6924@naver.com>
2025-08-02 02:15:11 +0900

    [BE] refactor: 초대코드 생성,조회,검증 및 입장 로직 리팩토링

--- backend-core/src/main/java/com/bbusyeo/voida/api/meetingroom/service/InviteCodeService.java
+++ backend-core/src/main/java/com/bbusyeo/voida/api/meetingroom/service/InviteCodeService.java
@@ -12,98 +12,143 @@ import com.bbusyeo.voida.global.redis.dao.RedisDao;
 import com.bbusyeo.voida.global.response.BaseResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.concurrent.ThreadLocalRandom;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.SecureRandom;
 import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class InviteCodeService {
 
-    // 키 충돌 방지와 유지보수를 위해 접두사 추가하여 관리, 초대코드 만료 시간 수정할 일 있으면 hours만 바꾸면 됨.
-    private static final String ROOM_ID_TO_INVITE_CODE_PREFIX = "room-id:";
-    private static final String INVITE_CODE_TO_ROOM_ID_PREFIX = "invite-code:";
-    private static final long INVITE_CODE_EXPIRATION_HOURS = 24;
-
-    private final RedisDao redisDao;
-    private final MeetingRoomRepository meetingRoomRepository;
-    private final MemberMeetingRoomRepository memberMeetingRoomRepository;
-    private final MemberRepository memberRepository;
-    private final MeetingRoomService meetingRoomService;
-
     public String createInviteCode(Long meetingRoomId) {
-        // 회의실 존재 여부 확인
+        // 대기실 존재 여부 확인
         meetingRoomRepository.findById(meetingRoomId)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_MEETING_ROOM));
 
-        String inviteCode;
-        String redisInviteKey;
+        // (확장성 고려) 재발급 시, 기존 발급된 초대 코드 있으면 찾아서 삭제
+        String oldRedisKey = ROOM_ID_TO_INVITE_CODE_PREFIX + meetingRoomId;
+        String oldInviteCode = (String) redisDao.getValue(oldRedisKey);
+        if (oldInviteCode != null) {
+            redisDao.deleteValue(INVITE_CODE_TO_ROOM_ID_PREFIX + oldInviteCode);
+            redisDao.deleteValue(oldRedisKey);
+        }
 
-        do {
 
-            int randomCode = ThreadLocalRandom.current().nextInt(100_000_000, 1_000_000_000);
-            inviteCode = String.valueOf(randomCode);
-            redisInviteKey = INVITE_CODE_TO_ROOM_ID_PREFIX + inviteCode;
+        // 유니크한 초대 코드 생성 (최대 5회 시도)
+        String uniqueInviteCode = null;
+        for (int i = 0; i < MAX_RETRY_COUNT; i++) {
+            String strNumberCode = generateStrNumberCode();
+            String redisInviteKey = INVITE_CODE_TO_ROOM_ID_PREFIX + strNumberCode;
 
-        } while (redisDao.getValue(redisInviteKey) != null); // 유일한 코드가 생성될 때까지 반복
+            // 생성된 코드가 이미 다른 대기실의 키로 존재하는지 확인
+            if (redisDao.getValue(redisInviteKey) == null) {
+                // 존재하지 않는다면, 이 코드를 유니크한 코드로 설정하고 종료
+                uniqueInviteCode = strNumberCode;
+                break;
+            }
+        }
 
+        // 5회 시도에도 유니크한 코드를 찾지 못했다면, 예외 처리
+        if (uniqueInviteCode == null) {
+            throw new BaseException(BaseResponseStatus.INVITE_CODE_GENERATION_FAILED);
+        }
+
+        // 초대 코드 저장 (24시간 유효)
         Duration expiration = Duration.ofHours(INVITE_CODE_EXPIRATION_HOURS);
-
-        // 초대코드 -> room ID 저장 (초대코드로 방 찾기 위해)
-        redisDao.setValue(redisInviteKey, meetingRoomId.toString(), expiration);
-
-        // room ID -> 초대코드 저장 (방장이 자기 방 코드 확인할 때 위해)
+        String redisInviteKey = INVITE_CODE_TO_ROOM_ID_PREFIX + uniqueInviteCode;
         String redisRoomKey = ROOM_ID_TO_INVITE_CODE_PREFIX + meetingRoomId;
-        redisDao.setValue(redisRoomKey, inviteCode, expiration);
-        return inviteCode;
+
+        // 초대코드를 키로, 대기실 ID를 저장 (참여 시 사용)
+        redisDao.setValue(redisInviteKey, meetingRoomId.toString(), expiration);
+        // 대기실 ID를 키로, 초대코드를 저장 (조회 시 사용)
+        redisDao.setValue(redisRoomKey, uniqueInviteCode, expiration);
+
+        return uniqueInviteCode;
     }
 
+    @Transactional(readOnly = true)
     public String getInviteCode(Long memberId, Long meetingRoomId) {
         // 방장 권한 확인
         meetingRoomService.checkHostAuthority(memberId, meetingRoomId);
 
+        // 대기실 ID를 키로 초대 코드 조회
         String redisKey = ROOM_ID_TO_INVITE_CODE_PREFIX + meetingRoomId;
         String inviteCode = (String) redisDao.getValue(redisKey);
 
+        // 초대 코드가 만료 됐으면 예외 처리
         if (inviteCode == null) {
             throw new BaseException(BaseResponseStatus.EXPIRED_INVITE_CODE);
         }
         return inviteCode;
     }
 
-    public void verifyInviteCodeAndJoin(Long memberId, String memberInviteCode) {
-
-        String redisInviteKey = INVITE_CODE_TO_ROOM_ID_PREFIX + memberInviteCode;
+    // 초대 코드 검증 및 참여
+    @Transactional
+    public void verifyInviteCodeAndJoin(Long memberId, String userInviteCode) {
+        // member가 입력한 초대 코드를 키로 Redis에 대기실 ID 조회
+        String redisInviteKey = INVITE_CODE_TO_ROOM_ID_PREFIX + userInviteCode;
         String roomIdStr = (String) redisDao.getValue(redisInviteKey);
 
-        // 초대 코드가 틀렸거나, 만료 되었을 경우
+        // 대기실 ID가 조회되지 않으면 코드가 틀렸거나 만료된것, 예외 처리
         if (roomIdStr == null) {
             throw new BaseException(BaseResponseStatus.INVALID_INVITE_CODE);
         }
         Long meetingRoomId = Long.parseLong(roomIdStr);
 
-        // member 정보와 대기실 정보 조회
-        // 서비스에 등록된 member가 아닌 경우
+        // 참여하려는 member가 존재하는지에 대해서와 참여 요청한 대기실이 존재하는지 확인
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.ILLEGAL_ARGUMENT));
 
-        // 입장하려고 하는 방이 없어졌을 경우
         MeetingRoom meetingRoom = meetingRoomRepository.findById(meetingRoomId)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_MEETING_ROOM));
 
-        // 대기실 정원 확인
+        // member가 이미 해당 대기실에 참여하고 있는지 확인
+        memberMeetingRoomRepository.findByMemberAndMeetingRoomId(member, meetingRoomId)
+                .ifPresent(m -> {
+                    // 이미 참여중이라면 예외 처리
+                    throw new BaseException(BaseResponseStatus.ALREADY_PARTICIPATING);
+                });
+
+        // 대기실 인원 확인
         if (meetingRoom.getMemberCount() >= 6) {
             throw new BaseException(BaseResponseStatus.MEETING_ROOM_FULL);
         }
 
-        // 대기실에 member 추가
-        MemberMeetingRoom plusParticipant = MemberMeetingRoom.builder()
+        // 모든 로직 통과했다면 대기실에 member 추가
+        MemberMeetingRoom newMember = MemberMeetingRoom.builder()
                 .member(member)
                 .meetingRoom(meetingRoom)
                 .state(MemberMeetingRoomState.PARTICIPANT)
                 .build();
-        memberMeetingRoomRepository.save(plusParticipant);
+        memberMeetingRoomRepository.save(newMember);
 
-        // 멤버 수 증가
+        // 대기실 현재 인원 수 1 증가
         meetingRoom.increaseMemberCount();
     }
+
+    // 입장 코드 생성 메서드
+    // 보안 고려해 SecureRandom, 메모리 성능 고려해 StringBuilder 사용
+    private String generateStrNumberCode() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(INVITE_CODE_LENGTH);
+        for (int i = 0; i < INVITE_CODE_LENGTH; i++) {
+            sb.append(ALPHABET_NUM.charAt(random.nextInt(ALPHABET_NUM.length())));
+        }
+        return sb.toString();
+    }
+
+    // 비즈니스 로직의 가독성을 위해 의존성과 상수 요소 하단에 작성
+    private static final String ROOM_ID_TO_INVITE_CODE_PREFIX = "room-id:";
+    private static final String INVITE_CODE_TO_ROOM_ID_PREFIX = "invite-code:";
+    private static final long INVITE_CODE_EXPIRATION_HOURS = 24;
+    private static final int MAX_RETRY_COUNT = 5;
+    private static final int INVITE_CODE_LENGTH = 9;
+    private static final String ALPHABET_NUM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    private final RedisDao redisDao;
+    private final MeetingRoomRepository meetingRoomRepository;
+    private final MemberMeetingRoomRepository memberMeetingRoomRepository;
+    private final MemberRepository memberRepository;
+    private final MeetingRoomService meetingRoomService;
 }
