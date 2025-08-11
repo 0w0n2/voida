05dbe4b2450f2fd59109c3e3393c664e1f9ebcfd
이석재 <lsj6924@naver.com>
2025-08-11 12:39:40 +0900

    [BE] refactor: 대기실 참여 인원 정보 조회에 유저 설정 추가

--- backend-core/src/main/java/com/bbusyeo/voida/api/member/repository/MemberSettingRepository.java
+++ backend-core/src/main/java/com/bbusyeo/voida/api/member/repository/MemberSettingRepository.java
@@ -3,8 +3,10 @@ package com.bbusyeo.voida.api.member.repository;
 import com.bbusyeo.voida.api.member.domain.MemberSetting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberSettingRepository extends JpaRepository<MemberSetting, Long> {
     MemberSetting findMemberSettingsByMemberId(Long memberId);
-
+    List<MemberSetting> findByMember_MemberUuidIn(List<String> memberUuids);
     void deleteMemberSettingByMemberId(Long memberId);
 }
