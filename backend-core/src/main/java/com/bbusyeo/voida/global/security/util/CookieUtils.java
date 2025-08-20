341689f730dbf626196694c972619cc717135886
이혜원 <dahxtq1@naver.com>
2025-08-20 13:59:45 +0900

    fix: cookie 다시 samesite=Lax 로 복원

--- backend-core/src/main/java/com/bbusyeo/voida/global/security/util/CookieUtils.java
+++ backend-core/src/main/java/com/bbusyeo/voida/global/security/util/CookieUtils.java
@@ -11,10 +11,9 @@ public class CookieUtils {
     public void setRefreshCookie(HttpServletResponse response, String refreshToken, int time) {
         ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", refreshToken) // 쿠키값을 null 로 설정
                 .httpOnly(true) // JavaScript
-                .secure(true)  // TODO-SECURITY: Https 사용하므로 개발 후엔 true로 (그런데 TCP 연결이라 false로 해달라는 요청이 있었음)
+                .secure(false)  // TODO-SECURITY: Https 사용하므로 개발 후엔 true로 (그런데 TCP 연결이라 false로 해달라는 요청이 있었음)
                 .path("/")
                 .maxAge(time) // maxAge를 0으로 설정하여 즉시 만료
-                .sameSite("None")
                 .build();
         response.addHeader("Set-Cookie", refreshCookie.toString()); // Response Headers에 Set-Cookie 항목이 보이고, Cookies 탭에도 나타남 // response.addCookie(refreshCookie);
     }
